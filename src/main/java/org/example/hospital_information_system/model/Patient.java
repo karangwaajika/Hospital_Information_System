a62737eceffac1e66a56aec7f9bb7fa94b4338ab
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-05-11 15:34:46 +0200

    feat: create patient class

---
+++ src/main/java/org/example/hospital_information_system/model/Patient.java
@@ -0,0 +1,79 @@
+package org.example.hospital_information_system.model;
+
+import java.time.LocalDate;
+
+public class Patient {
+    private int id;
+    private String firstName;
+    private String surname;
+    private String telephoneNumber;
+    private char sex;
+    private LocalDate dateOfBirth;
+
+    public Patient(String firstName, String surname,
+                   String telephoneNumber, char sex, LocalDate dateOfBirth){
+        this.firstName = firstName;
+        this.surname = surname;
+        this.telephoneNumber = telephoneNumber;
+        this.sex = sex;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Patient(int id,String firstName, String surname,
+                   String telephoneNumber, char sex, LocalDate dateOfBirth){
+        this.id = id;
+        this.firstName = firstName;
+        this.surname = surname;
+        this.telephoneNumber = telephoneNumber;
+        this.sex = sex;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
